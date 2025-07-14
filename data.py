e4245223e8a56aafb4058dc877aea61cf3d0ca83
vichShir <victor.shirasuna@gmail.com>
2025-07-13 21:29:44 -0300

    Updated Deep-MINE

--- data.py
+++ data.py
@@ -20,12 +20,36 @@ from PIL import Image
 
 class Encoder():
 
-    def __init__(self, df_feedbacks, df_products, df_users, description_vocab, review_vocab):
+    def __init__(self, 
+                df_feedbacks, 
+                df_products, 
+                df_users, 
+                description_vocab, 
+                review_vocab, 
+                description_key='description',
+                review_key='reviewText',
+                images_path='./data/books/images/',
+                users_index_key='index', 
+                users_id_key='reviewerID', 
+                item_id_key='asin', 
+                item_img_key='imageURL'
+                ):
         self.df_feedbacks = df_feedbacks
         self.df_products = df_products
         self.df_users = df_users
+
         self.description_vocab = description_vocab
         self.review_vocab = review_vocab
+
+        self.description_key = description_key
+        self.review_key = review_key
+
+        self.images_path = images_path
+
+        self.users_index_key = users_index_key
+        self.users_id_key = users_id_key
+        self.item_id_key = item_id_key
+        self.item_img_key = item_img_key
 
     def process(self, batch):
         u_ls = []
@@ -42,31 +66,31 @@ class Encoder():
             i = data[1]
             j = data[2]
 
-            user_key = self.df_users[self.df_users['index'] == u]['reviewerID'].iloc[0]
-            pos_feedback = self.df_feedbacks[self.df_feedbacks['reviewerID'] == user_key].iloc[0]
+            user_key = self.df_users[self.df_users[self.users_index_key] == u][self.users_id_key].iloc[0]
+            pos_feedback = self.df_feedbacks[self.df_feedbacks[self.users_id_key] == user_key].iloc[0]
 
-            image_name = os.path.basename(pos_feedback['imageURL'])
-            p_image = load_image(os.path.join('./data/images/', image_name))
-            p_description = self.description_vocab.transform(pos_feedback['description']).squeeze(0).float()
-            p_review = self.review_vocab.transform(pos_feedback['reviewText']).squeeze(0).float()
+            image_name = os.path.basename(pos_feedback[self.item_img_key])
+            p_image = load_image(os.path.join(self.images_path, image_name))
+            p_description = self.description_vocab.transform(pos_feedback[self.description_key]).squeeze(0).float()
+            p_review = self.review_vocab.transform(pos_feedback[self.review_key]).squeeze(0).float()
 
             imgs = []
             descs = []
             revs = []
             for neg_item_id in j:
-                item_key = self.df_products[self.df_products['index'] == neg_item_id]['asin'].iloc[0]
+                item_key = self.df_products[self.df_products[self.users_index_key] == neg_item_id][self.item_id_key].iloc[0]
 
                 try:
-                    neg_feedback = self.df_feedbacks[self.df_feedbacks['asin'] == item_key].sample(1).iloc[0]
+                    neg_feedback = self.df_feedbacks[self.df_feedbacks[self.item_id_key] == item_key].sample(1).iloc[0]
 
-                    image_name = os.path.basename(neg_feedback['imageURL'])
-                    n_image = load_image(os.path.join('./data/images/', image_name))
-                    n_description = self.description_vocab.transform(neg_feedback['description']).squeeze(0).float()
-                    n_review = self.review_vocab.transform(neg_feedback['reviewText']).squeeze(0).float()
+                    image_name = os.path.basename(neg_feedback[self.item_img_key])
+                    n_image = load_image(os.path.join(self.images_path, image_name))
+                    n_description = self.description_vocab.transform(neg_feedback[self.description_key]).squeeze(0).float()
+                    n_review = self.review_vocab.transform(neg_feedback[self.review_key]).squeeze(0).float()
                 except:
-                    n_image = torch.zeros(3, 40, 40)
-                    n_description = torch.zeros((12916,))
-                    n_review = torch.zeros((29678,))
+                    n_image = torch.zeros(3, 40, 40)  # TODO: hardcoded
+                    n_description = torch.zeros((self.description_vocab.vocab_size(),))
+                    n_review = torch.zeros((self.review_vocab.vocab_size(),))
 
                 imgs.append(n_image.unsqueeze(0))
                 descs.append(n_description.unsqueeze(0))
@@ -108,11 +132,16 @@ def load_image(path, resize=(40, 40)):
 
 class DeepMINEDataset(Dataset):
 
-    def __init__(self, df_feedbacks, df_products, df_users, n_neg_items=1):
+    def __init__(self, df_feedbacks, df_products, df_users, n_neg_items=1, users_index_key='index', users_id_key='reviewerID', item_id_key='asin'):
         self.df_feedbacks = df_feedbacks
         self.df_products = df_products
         self.df_users = df_users
         self.n_neg_items = n_neg_items
+
+        self.users_index_key = users_index_key
+        self.item_index_key = users_index_key
+        self.users_id_key = users_id_key
+        self.item_id_key = item_id_key
 
     def __len__(self):
         return self.df_feedbacks.shape[0]
@@ -120,18 +149,20 @@ class DeepMINEDataset(Dataset):
     def __getitem__(self, idx):
         try:
             pos_feedback = self.df_feedbacks.iloc[idx]
-            u = self.df_users[self.df_users['reviewerID'] == pos_feedback['reviewerID']]['index'].iloc[0]
-            i = self.df_products[self.df_products['asin'] == pos_feedback['asin']]['index'].iloc[0]
+            u = self.df_users[self.df_users[self.users_id_key] == pos_feedback[self.users_id_key]][self.users_index_key].iloc[0]
+            i = self.df_products[self.df_products[self.item_id_key] == pos_feedback[self.item_id_key]][self.item_index_key].iloc[0]
 
-            user_key = pos_feedback['reviewerID']
-            user_products = list(self.df_feedbacks.query("reviewerID == @user_key")['asin'].unique())  # list all products user has a feedback
+            user_key = pos_feedback[self.users_id_key]
+            # user_products = list(self.df_feedbacks.query(f"{self.users_id_key} == @user_key")[self.item_id_key].unique())  # list all products user has a feedback
+            user_products = list(self.df_feedbacks[self.df_feedbacks[self.users_id_key] == user_key][self.item_id_key].unique())  # list all products user has a feedback
             found_neg_item = False
             # while found_neg_item is False:
-            n_item_key = self.df_products.query("asin != @user_products").sample(self.n_neg_items)['asin'].to_list()  # get a random negative product
-            all_neg_feedbacks = self.df_feedbacks[self.df_feedbacks['asin'].isin(n_item_key)]
-                # if all_neg_feedbacks.shape[0] > 0 and all_neg_feedbacks['asin'].nunique() == self.n_neg_items:
+            n_item_key = self.df_products.query(f"{self.item_id_key} != @user_products").sample(self.n_neg_items)[self.item_id_key].to_list()  # get a random negative product
+            # n_item_key = self.df_products[self.df_products[self.item_id_key] != user_products[0]].sample(self.n_neg_items)[self.item_id_key].to_list()  # get a random negative product
+            all_neg_feedbacks = self.df_feedbacks[self.df_feedbacks[self.item_id_key].isin(n_item_key)]
+                # if all_neg_feedbacks.shape[0] > 0 and all_neg_feedbacks[self.item_id_key].nunique() == self.n_neg_items:
                 #     found_neg_item = True
-            j = self.df_products[self.df_products['asin'].isin(n_item_key)]['index'].to_list()
+            j = self.df_products[self.df_products[self.item_id_key].isin(n_item_key)][self.item_index_key].to_list()
         except Exception as e:
             print('FAILED data loader:', e)
             import ipdb; ipdb.set_trace()
